f51a83513b996e867c668992d24f2028d18d9604
kamallochan2004 <theultimatesource2019@gmail.com>
2023-10-16 12:19:20 +0530

    Q3_1618

--- Q3_1618.java
+++ Q3_1618.java
@@ -1,5 +1,10 @@
+/*
+ * Name: Kamallochan Das
+ * Reg No: 2241004066
+ * PS LINK: https://cses.fi/problemset/task/1083/
+ */
 import java.util.*;
-public class Q3 {
+public class Q3_1618 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
