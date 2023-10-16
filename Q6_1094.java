3a0dde4b4bc4cec2545003e25958a3ae736ba5e3
kamallochan2004 <theultimatesource2019@gmail.com>
2023-10-16 19:58:52 +0530

    Q6_1094

---
+++ Q6_1094.java
@@ -0,0 +1,28 @@
+/*
+ * Name: Kamallochan Das
+ * Reg No: 2241004066
+ * PS LINK: https://cses.fi/problemset/task/1094/
+ */
+
+import java.util.Scanner;
+
+public class Q6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        long[] array = new long[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextLong();
+        }
+        long moves = 0;
+        for (int i = 1; i < n; i++) {
+            if (array[i] < array[i - 1]) {
+                moves += array[i - 1] - array[i];
+                array[i] = array[i - 1];
+            }
+        }
+        System.out.println(moves);
+        sc.close();
+    }
+}
