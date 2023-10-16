19dff2989e13a173bc4056e8b316bf98ae154bf8
kamallochan2004 <theultimatesource2019@gmail.com>
2023-10-16 10:50:09 +0530

    Q1_1069

---
+++ Q1_1069.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Q1_1069 {
+    public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.next();
+            int longestRepetition = repetition(input);
+            System.out.println(longestRepetition);
+        }
+        public static int repetition(String sequence) {
+            int longest = 1;
+            int current = 1;
+            for (int i = 1; i < sequence.length(); i++) {
+                if (sequence.charAt(i) == sequence.charAt(i - 1)) {
+                    current++;
+                } else {
+                    current = 1;
+                }
+                if (current > longest) {
+                    longest = current;
+                }
+            }
+            return longest;
+        }
+    }
