7acb840b744216b483ecd654726c7cf6317758ef
kamallochan2004 <theultimatesource2019@gmail.com>
2023-10-16 12:32:17 +0530

    Q3_1618

--- Q3_1618.java
+++ Q3_1618.java
@@ -1,7 +1,7 @@
 /*
  * Name: Kamallochan Das
  * Reg No: 2241004066
- * PS LINK: https://cses.fi/problemset/task/1083/
+ * PS LINK: https://cses.fi/problemset/task/1618/
  */
 import java.util.*;
 public class Q3_1618 {
